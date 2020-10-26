52bd29a8944abf48f480cbf387832a391e34be2c
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-27 02:05:23 +0300

    add delete card prod func

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
+const cardRoutes = require('./routes/card');
 const coursesRoutes = require('./routes/courses');
 
 const app = express()
@@ -17,11 +18,12 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
 
 const PORT = process.env.PORT || 3000;
 
