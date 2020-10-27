d884c5c0906e5670c9e586d291384b674977b836
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-27 17:52:51 +0300

    connected mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const cardRoutes = require('./routes/card');
@@ -13,8 +16,10 @@ const hbs = exphbs.create({
 	extname: 'hbs'
 });
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
+app.engine('handlebars', exphbs({
+	handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
+app.set('view engine', 'handlebars');
 app.set('views', 'views')
 
 
@@ -27,6 +32,21 @@ app.use('/card', cardRoutes)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-	console.log(`Сервер запущен на порту ${PORT}`);
-})
+async function start() {
+	try {
+		const url = 'mongodb+srv://vgvsshi:NllK0t7i67NykcLE@cluster0.3lg5l.mongodb.net/shop';
+		await mongoose.connect(url, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false
+		})
+		app.listen(PORT, () => {
+			console.log(`Сервер запущен на порту ${PORT}`);
+		})
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+start()
+
