1a3cc87852738730a535ff9acb756f70e4f92fe0
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-28 15:36:18 +0300

    add session

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const Handlebars = require('handlebars')
+const session = require('express-session');
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
@@ -11,6 +12,7 @@ const ordersRoutes = require('./routes/orders');
 const coursesRoutes = require('./routes/courses');
 const authRoutes = require('./routes/auth');
 const User = require('./models/user');
+const varMiddleware = require('./middleware/variables');
 
 const app = express()
 
@@ -37,6 +39,14 @@ app.use(async (req, res, next) => {
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+	secret: 'some secret value',
+	resave: false,
+	saveUninitialized: false
+}))
+app.use(varMiddleware)
+
+
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
