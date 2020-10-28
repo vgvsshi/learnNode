65efbf0164afbe28a5a83c9e3eb93f77a9ebb6bf
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-28 20:20:03 +0300

    add authentication

--- routes/auth.js
+++ routes/auth.js
@@ -16,15 +16,53 @@ router.get('/logout', async (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-	const user = await User.findById('5f983888b25265438cc62ebc');
-	req.session.user = user
-	req.session.isAuthenticated = true
-	req.session.save(err => {
-		if (err) {
-			throw err
+	try {
+		const { email, password } = req.body
+		const candidate = await User.findOne({ email })
+
+		if (candidate) {
+			const isSame = password === candidate.password
+			if (isSame) {
+				const user = candidate;
+				req.session.user = user
+				req.session.isAuthenticated = true
+				req.session.save(err => {
+					if (err) {
+						throw err
+					}
+					res.redirect('/');
+				})
+			} else {
+				res.redirect('/auth/login#login')
+			}
+		} else {
+			res.redirect('/auth/login#login')
 		}
-		res.redirect('/');
-	})
+	} catch (e) {
+		console.log(e)
+	}
+})
+
+router.post('/register', async (req, res) => {
+	try {
+		const { email, password, confirm, name } = req.body
+		const candidate = await User.findOne({ email })
+
+		if (candidate) {
+			res.redirect('/auth/login#register')
+		} else {
+			const user = new User({
+				email,
+				password,
+				name,
+				cart: { items: [] }
+			})
+			await user.save()
+			res.redirect('/auth/login#login')
+		}
+	} catch (e) {
+		console.log(e)
+	}
 })
 
 module.exports = router
