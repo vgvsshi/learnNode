760e719b3a2990a837024f481a65978952af971f
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-30 21:29:18 +0300

    added file upload

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ const cardRoutes = require('./routes/card');
 const ordersRoutes = require('./routes/orders');
 const coursesRoutes = require('./routes/courses');
 const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile')
 const varMiddleware = require('./middleware/variables');
 const userMiddleware = require('./middleware/user')
+const errorHandler = require('./middleware/error');
+const fileMiddleware = require('./middleware/file')
 const keys = require('./keys')
 
 
@@ -45,6 +48,7 @@ app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views/')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
 	secret: keys.SESSION_SECRET,
@@ -52,6 +56,7 @@ app.use(session({
 	saveUninitialized: false,
 	store
 }))
+app.use(fileMiddleware.single('avatar'))
 app.use(csurf())
 app.use(flash())
 app.use(varMiddleware)
@@ -64,6 +69,9 @@ app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
 app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000;
 
