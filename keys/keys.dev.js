2a414c3ecffc62d0fa00efd0e0a9405c1f4d5fdc
Amir <71263408+vgvsshi@users.noreply.github.com>
2020-10-31 20:33:25 +0300

    add keys protection

---
+++ keys/keys.dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+	MONGODB_URI: 'mongodb+srv://vgvsshi:787898Amir@cluster0.3lg5l.mongodb.net/shop',
+	SESSION_SECRET: 'some secret value',
+	API_KEY: '8c50ed6890c6d78abf958188dcacd756-us2',
+	BASE_URL: 'http://localhost:3000'
+}
---
+++ keys/keys.prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+	MONGODB_URI: process.env.MONGODB_URI,
+	SESSION_SECRET: process.env.SESSION_SECRET,
+	API_KEY: process.env.API_KEY,
+	BASE_URL: process.env.BASE_URL
+}
